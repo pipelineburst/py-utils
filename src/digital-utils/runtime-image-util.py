8b4a1465d6dbc4afc0a783c5c973ab4b6a916164
Dirk Michel <michel.dirk@googlemail.com>
2023-12-19 17:52:37 +0000

    moved to functions

--- src/digital-utils/runtime-image-util.py
+++ src/digital-utils/runtime-image-util.py
@@ -1,144 +1,139 @@
+#!/usr/bin/python
+
 import argparse
-
-# starting with argparse to determine what the user wants to do
-argParser = argparse.ArgumentParser()
-argParser.add_argument("-l", "--list", action="store_true", help="getting the list of deployed container imgages")
-argParser.add_argument("-s", "--size", action="store_true", help="getting the size for the deployed container imgages")
-argParser.add_argument("-u", "--ubi", action="store_true", help="getting the ubi comliance status for the deployed container imgages")
-argParser.add_argument("-v", "--vuln", action="store_true", help="getting the cve finding summary for the deployed container imgages")
-argParser.add_argument("-a", "--all", action="store_true", help="getting all reports... container image list && size && vuln && ubi")
-args = argParser.parse_args()
-
-if args.all == True:
-    print("### Generating ALL the image reports ###")   
-elif args.list == True:
-    print("### Generating the LIST of deployed images ###")   
-elif args.size == True:
-    print("### Generating the SIZE report ###")
-elif args.vuln == True:
-    print("### Generating the VULN findings count report ###")
-elif args.ubi == True:
-    print("### Generating the UBI compliance report ###")     
-else:
-    print("No option provided... What should we do?")
-    print("Please use -h to find the available options") 
-    exit(1)
-
-# importing the libraries now. this makes the initial argparse faster
-import boto3
-import kubernetes as k8s
-import pandas as pd
-import docker
+from enum import unique
 import json
 import os
 
-# resetting files
-print("### Pre-Flight Checks and Prep ###")
-print("Start resetting processing files...")
-print("Clearing file...")
+# starting with argparse to determine what the user wants to do
 
-try:
-    file = open("image_list.txt", "r+")
-    file.truncate()
-    file = open("image_list_uniq.txt", "r+")
-    file.truncate() 
-except Exception as e:
-    file = open("image_list.txt", "w+")
-    file = open("image_list_uniq.txt", "w+")
-
-print("Done with resetting files !")
+def get_args():
     
-# getting running container list from k8s cluster 
-print("Getting the running images from the k8s cluster")
-print("This takes about 10s...")
+    parser = argparse.ArgumentParser(description="This is a script to get the list of deployed container images")
+    parser.add_argument("-l", "--list", action="store_true", help="getting the list of deployed container imgages")
+    parser.add_argument("-s", "--size", action="store_true", help="getting the size for the deployed container imgages")
+    parser.add_argument("-u", "--ubi", action="store_true", help="getting the ubi comliance status for the deployed container imgages")
+    parser.add_argument("-v", "--vuln", action="store_true", help="getting the cve finding summary for the deployed container imgages")
+    parser.add_argument("-a", "--all", action="store_true", help="getting all reports... container image list && size && vuln && ubi")
+    args = parser.parse_args()
+    return args
 
-k8s.config.load_kube_config()
-v1 = k8s.client.CoreV1Api()
-response_k8s = v1.list_namespaced_pod(namespace="eaa", watch=False)
-for pod in response_k8s.items:
-    hs = open("image_list.txt","a")
-    hs.write(f"{pod.spec.containers[0].image}" + "\n")
-    hs.close() 
+# run functions based on user arguments
 
-print("OK, got the list of deployed images !")
+def parse_args(args):
 
-# load file as list and remove duplicates
-print("Dedupliacting and creating the list of uniq running images...")
+    if args.all == True:
+        print("### Generating the ALL reports ###")  
+        generate_image_list()
+        list_images()
+        size_images()
+        vuln_images()
+        ubi_images()
+    elif args.list == True:
+        print("### Generating the LIST of deployed images ###")  
+        generate_image_list()
+        list_images()
+    elif args.size == True:
+        print("### Generating the SIZE report ###")
+        generate_image_list()    
+        size_images()
+    elif args.vuln == True:
+        print("### Generating the VULN findings count report ###")
+        generate_image_list()    
+        vuln_images()
+    elif args.ubi == True:
+        print("### Generating the UBI compliance report ###")
+        generate_image_list()    
+        ubi_images()    
+    else:
+        print("No option provided... What should we do?")
+        print("Please use -h to find the available options") 
+        exit(1)
 
-with open('image_list.txt') as image_list:
-    images = image_list.read().splitlines()
-    images = list(dict.fromkeys(images))
-    hs = open("image_list_uniq.txt","a")
-    for image in images:
-        hs.write(f"{image}" + "\n")
-    hs.close()
+def generate_image_list():
 
-print("OK, got the uniq list of deployed images!")
+    print("Connecting to the k8s cluster to obtain the list of running images")
+    print("This takes about 10s...")
 
-#load file and remove non-mycom images from the list to be able to get the size of the images that are running on the k8s cluster
-print("Removing non-mycom images from the list to be able to get the size of the images that are running on the k8s cluster")
+    try:
+        file = open("image_list.txt", "r+")
+        file.truncate()    
+    except Exception as e:
+        file = open("image_list.txt", "w+")  
 
-with open('image_list_uniq.txt') as image_list:
-    images = image_list.read().splitlines()
-    hs = open("image_list_uniq.txt","w")
-    for image in images:
-        if "docker.mycom-osi.com" in image:
-            hs.write(f"{image}" + "\n")
-    hs.close()
+    import kubernetes as k8s
+    try:
+        k8s.config.load_kube_config()
+        v1 = k8s.client.CoreV1Api()
+        response_k8s = v1.list_namespaced_pod(namespace="eaa", watch=False)
+        for pod in response_k8s.items:
+            hs = open("image_list.txt","a")
+            hs.write(f"{pod.spec.containers[0].image}" + "\n")
+            hs.close() 
+        with open('image_list.txt') as image_list:
+            images = image_list.read().splitlines()
+            images = list(dict.fromkeys(images))
+            hs = open("image_list_uniq.txt","a")
+            for image in images:
+                hs.write(f"{image}" + "\n")
+            hs.close()
 
-print('OK, removed non-mycom images')
-print("### Completed Pre-Flight Checks and Prep ###")
-print("### Ready to go ! ###")
+        print("Removing non-mycom images from the list to be able to get the size of the images that are running on the k8s cluster")
 
-if args.list == True or args.all == True:
+        with open('image_list_uniq.txt') as image_list:
+            images = image_list.read().splitlines()
+            hs = open("image_list_uniq.txt","w")
+            for image in images:
+                if "docker.mycom-osi.com" in image:
+                    hs.write(f"{image}" + "\n")
+            hs.close()
+    except k8s.client.exceptions.ApiException:
+        print("You are not logged into the cluster. Please authenticate first before you use the util.")
+        exit(1)
+    except Exception as e:
+        print(e)
+        print("You probably dont have a valid .kube/config file on your system. Please check before you use the util")
+        exit(1)
+
+def list_images():
     
     print("##############################################")
     print("Done ! Result file: image_list_uniq.txt")
-    print("##############################################")   
+    print("##############################################")
 
-if args.size == True or args.all == True:
+def size_images():
+    
+    import boto3
+    import pandas as pd
     
     print("##############################################")
     print("Let's get the image size report...")
     print("##############################################")
 
-    print("Start resetting size files...")
-    print("Clearing files...")
-    try:
-        file = open("image_size.txt", "r+")
-        file.truncate()
-        file = open("image_size_sorted.txt", "r+")
-        file.truncate()
-    except Exception as e:
-        file = open("image_size.txt", "w+")
-        file = open("image_size_sorted.txt", "w+")
-    print("OK, done resetting size files !")
-
     region = 'eu-west-1'
     client = boto3.client('ecr')
 
-    print("Now getting the image sizes from ECR")
+    print("Getting the image sizes from ECR")
     print("Printing image sizes to stdout...")
     print("This takes about 60s...")
 
     with open('image_list_uniq.txt') as container_list:
         containers = container_list.read().splitlines()
         try: 
-            for container in containers:
-                repo = container.replace("docker.mycom-osi.com/", "").split(":")[0]
-                tag = container.split(":")[-1]
-                response = client.describe_images(
-                        repositoryName=repo,
-                        imageIds=[
-                            {
-                                'imageTag': tag
-                            },
-                        ]
-                )
-                hs = open("image_size.txt","a")
-                hs.write(f"{repo}:{tag} size = {round(int(response['imageDetails'][0]['imageSizeInBytes'])/1e6)} MB"  + "\n")
-                hs.close() 
-                print(f"{repo}:{tag} size = {round(int(response['imageDetails'][0]['imageSizeInBytes'])/1e6)} MB")
+            with open("image_size.txt", "w") as hs:
+                for container in containers:
+                    repo = container.replace("docker.mycom-osi.com/", "").split(":")[0]
+                    tag = container.split(":")[-1]
+                    response = client.describe_images(
+                            repositoryName=repo,
+                            imageIds=[
+                                {
+                                    'imageTag': tag
+                                },
+                            ]
+                    )
+                    hs.write(f"{repo}:{tag} size = {round(int(response['imageDetails'][0]['imageSizeInBytes'])/1e6)} MB"  + "\n")
+                    print(f"{repo}:{tag} size = {round(int(response['imageDetails'][0]['imageSizeInBytes'])/1e6)} MB")
         except Exception as e:
             print("Oh no !")
             print(e)
@@ -153,31 +148,28 @@ if args.size == True or args.all == True:
     print("Done ! Result file: image_size_sorted.txt")
     print("##############################################")
 
-if args.vuln == True or args.all == True:
+def vuln_images():
+
+    import boto3
 
     print("##############################################")
     print("Let's get the vulnerability count report...")
-    print("##############################################")
-
-    print("Start resetting vuln files...")
-    print("Clearing files...")
+    print("##############################################") 
     
-    try:
-        file = open("image_vulns.txt", "r+")
-        file.truncate()
-    except Exception as e:
-        file = open("image_vulns.txt", "w+")  
-
-    print("Done with resetting files !")
-
     region = 'eu-west-1'
     client = boto3.client('ecr')
+
+    try:
+        file = open("image_vulns.txt", "r+")
+        file.truncate()    
+    except Exception as e:
+        file = open("image_vulns.txt", "w+")  
 
     print("Now getting the image vulns from ECR")
     print("Printing images with vulns to stdout...")
     print("This takes about 60s...")
 
-    with open('image_list_uniq.txt') as container_list:
+    with open('image_list_uniq.txt', "r") as container_list:
         containers = container_list.read().splitlines()
         try: 
             critical = 0
@@ -215,30 +207,32 @@ if args.vuln == True or args.all == True:
             print("Oh no !")
             print(e)
             
-if args.ubi == True or args.all == True:
-
+def ubi_images():
+    
+    import docker
+    
     print("##############################################")
     print("Let's get the ubi compliance report...")
     print("##############################################")
-
-    print("Start resetting ubi processing files...")
-    print("Clearing files...")
+    
     try:
         file = open("image_ubi.txt", "r+")
         file.truncate()    
     except Exception as e:
         file = open("image_ubi.txt", "w+")  
 
-    print("Done with resetting ubi files !")
-    print("Now getting image UBI compliance")
-    print("Printing non-compliant images to stdout...")
-    print("This takes about 60s...")
-    
+    print("Logging into the docker.mycom-osi.com container registry")
+
     client = docker.from_env()
     client.login(username="mycomosi", password="mo-eaa-ecr", registry="docker.mycom-osi.com")
 
+    # the sha's of the lates ubi images
     ubi8_minimal_8_9_1029 = "sha256:8f42ad26ccdae7ec04dac9501e3c011a88c8663559699974ecf1697999914f0d"
     ubi8python39_1_155 = "sha256:d938174480191f5a4b9117a3a4ef6e22a48572994b50b71f863610a82b55d371"
+
+    print("Getting the container image configs from the registry...")
+    print("Printing non-compliant images to stdout...")
+    print("This takes about 60s...")
 
     with open('image_list_uniq.txt') as container_list:
             containers = container_list.read().splitlines()
@@ -283,3 +277,10 @@ if args.ubi == True or args.all == True:
             except Exception as e:
                 print("Oh no !")
                 print(e)
+
+def main():
+    args = get_args()
+    parse_args(args)
+    
+if __name__ == "__main__":
+    main()
